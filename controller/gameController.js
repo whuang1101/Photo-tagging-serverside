9ae780e466963942577fc75a914a4321acd19aa3
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-08-29 18:27:37 -0400

    Change get for route /

--- controller/gameController.js
+++ controller/gameController.js
@@ -3,6 +3,5 @@ const Game = require("../models/game");
 
 
 exports.get = asyncHandler(async(req, res, next) => {
-    const game = await Game.find().populate("characters").exec();
-    res.send(game);
+    res.send("nothing here")
 })
