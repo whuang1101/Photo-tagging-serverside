7b423c6e5f5e59629b11046d273d230130998b16
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-08-29 18:22:53 -0400

    Add Express for Railway

--- controller/characterController.js
+++ controller/characterController.js
@@ -3,7 +3,6 @@ const Character = require("../models/character");
 
 
 exports.get = asyncHandler(async(req, res, next) => {
-    console.log("no")
     const character = await Character.findOne({name: req.params.id}).exec();
     if(character)
    { res.json({x:character.x, y:character.y});}
